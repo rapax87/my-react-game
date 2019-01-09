b6f7fcd0529de47a77eb67c87ec793e64502eeff
Bingbing Xu <bingbing.xu@elmosoftware.com.au>
2019-01-09 14:02:33 +1100

    game start

--- src/App.js
+++ src/App.js
@@ -25,4 +25,6 @@ class App extends Component {
   }
 }
 
+
 export default App;
+
---
+++ src/ShoppingList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+
+
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+// Example usage: <ShoppingList name="Mark" />
+
+
+
+export default ShoppingList;
