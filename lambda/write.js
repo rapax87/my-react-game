efa1e2bcfe34cff4cc3bee93fd08ad08031286c3
Bingbing Xu <bingbing.xu@elmosoftware.com.au>
2019-01-11 11:11:04 +1100

    always write id 1

--- lambda/write.js
+++ lambda/write.js
@@ -1,20 +1,21 @@
 'use strict';
 
 var AWS = require('aws-sdk'),
-	uuid = require('uuid'),
-	documentClient = new AWS.DynamoDB.DocumentClient(); 
+    uuid = require('uuid'),
+    documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.writeGame = function(event, context, callback){
-	var params = {
-		Item : {
-			"id" : uuid.v1(),
-			"state" : event.state
-		},
-		TableName : process.env.TABLE_NAME
-	};
-	documentClient.put(params, function(err, data){
-		callback(err, data);
-	});
+    var params = {
+        Item : {
+            // "id" : uuid.v1(),
+            "id" : "1",
+            "state" : event.state
+        },
+        TableName : process.env.TABLE_NAME
+    };
+    documentClient.put(params, function(err, data){
+        callback(err, data);
+    });
 }
 
 // node.js.6.10
